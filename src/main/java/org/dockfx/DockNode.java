d083371e89b7bd154b4b5152598966e138bab612
Ahmed Abumarasa <ahmeda@connectpoint.local>
2017-01-20 15:31:07 +0100

    make the floating windows to waiting the main window to decide if they can be closed or not in case of closing them by clsoing the main window

--- src/main/java/org/dockfx/DockNode.java
+++ src/main/java/org/dockfx/DockNode.java
@@ -360,15 +360,13 @@ public class DockNode extends VBox implements EventHandler<MouseEvent> {
 
       stage = new Stage();
 
-      dockPane.getScene().getWindow().addEventFilter(
-        WindowEvent.WINDOW_CLOSE_REQUEST,
-        new EventHandler<WindowEvent>()
-        {
-          @Override
-          public void handle(WindowEvent event) {
-            stage.close();
-          }
-        });
+		EventHandler<WindowEvent> mainWindowCloseHandler = dockPane.getScene().getWindow().getOnCloseRequest();
+		dockPane.getScene().getWindow().setOnCloseRequest(event -> {
+			mainWindowCloseHandler.handle(event);
+			if (!event.isConsumed()) {
+				stage.close();
+			}
+		});
 
       stage.titleProperty().bind(titleProperty);
       if (dockPane != null && dockPane.getScene() != null
